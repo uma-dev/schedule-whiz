466d46913bdaed2b13fe29c51a9d533cc2d31596
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-11-08 17:53:12 -0600

    Added mvn project struture

---
+++ src/main/java/com/umadev/schedulewhiz/SchedulewhizApplication.java
@@ -0,0 +1,13 @@
+package com.umadev.schedulewhiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulewhizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulewhizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umadev/schedulewhiz/SchedulewhizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umadev.schedulewhiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulewhizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

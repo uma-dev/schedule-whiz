7dd565c574717c686ee2ef139b12256554256514
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-11-12 17:12:28 -0600

    Removed test temporary

--- src/test/java/com/umadev/schedulewhiz/SchedulewhizApplicationTests.java
+++ src/test/java/com/umadev/schedulewhiz/SchedulewhizApplicationTests.java
@@ -3,7 +3,7 @@ package com.umadev.schedulewhiz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SchedulewhizApplicationTests {
 
 	@Test

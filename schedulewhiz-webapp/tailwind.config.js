dc532b1a706ae274c2fd9e9114e2a06c1042f86b
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-11-24 23:06:44 -0600

    Added custom colors

--- schedulewhiz-webapp/tailwind.config.js
+++ schedulewhiz-webapp/tailwind.config.js
@@ -6,8 +6,17 @@ export default {
       colors: {
         "my-grey": "#F5F5F7",
         "my-blue": "#0C356A",
+        "yellow-alert": "#FFF0CE",
+        "yellow-schedulewhiz": "#EBB200",
+        "black-stone": "#0C0B0B",
       },
     },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(my-grey|my-blue|yellow-alert|black-stone|yellow-schedulewhiz)/,
+    },
+  ],
 };

1307eb7fec57753441d92b93ddf293902d4886e7
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-12-26 08:45:42 -0600

    Fixed deprecated spring sec methods

--- backend/src/main/java/com/umadev/schedulewhiz/security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/umadev/schedulewhiz/security/config/SecurityConfiguration.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-// @RequiredArgsConstructor
 public class SecurityConfiguration {
 
   private JwtAuthenticationFilter jwtAuthFilter;
@@ -27,18 +28,18 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
-        .authorizeHttpRequests()
-        // White list all the Login controller endpoints
-        .requestMatchers("/api/auth/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
+    http.cors(Customizer.withDefaults()) // Enable CORS in spring security
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            (authorize) ->
+                authorize
+                    // White list all the Login controller endpoints
+                    .requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 

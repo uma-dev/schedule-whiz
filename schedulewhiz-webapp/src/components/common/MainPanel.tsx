abbc6baef95101f5bca09cddcc58f3ab3277b570
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-12-27 23:25:56 -0600

    Fixed subtitles in card

--- schedulewhiz-webapp/src/components/common/MainPanel.tsx
+++ schedulewhiz-webapp/src/components/common/MainPanel.tsx
@@ -37,7 +37,7 @@ const MainPanel = ({ employeeId }: Props) => {
             <h2 className="font-semibold">{`Hello ${employee.names}!`}</h2>
             <span>Its good to see you again</span>
           </div>
-          <div className="flex items-center max-w-xs flex-1 h-fit justify-center">
+          <div className="flex items-center max-w-xs flex1 h-fit justify-center">
             <img src={logo} className="max-w-[60%] h-fit max-h-14" alt="Logo" />
           </div>
         </div>
@@ -45,21 +45,25 @@ const MainPanel = ({ employeeId }: Props) => {
 
       <Card color="my-grey flex flex-row items-center gap-4 justify-center">
         <div className="flex flex-col items-center">
-          <i className='bx bx-calendar bx-md'></i>
+          <i className="bx bx-calendar bx-md"></i>
         </div>
         <div className="flex flex-col items-center">
+          {employee.schedule != null && (
             <h3 className="font-bold">{`${employee.schedule.name}`}</h3>
-            <span className="text-xs">Weekly agenda</span>
+          )}
+          <span className="text-xs">Weekly agenda</span>
         </div>
-     </Card>
+      </Card>
 
       <Card color="my-grey flex flex-row items-center gap-4 justify-center">
         <div className="flex flex-col items-center">
-          <i className='bx bx-bowl-hot bx-md'></i>
+          <i className="bx bx-bowl-hot bx-md"></i>
         </div>
         <div className="flex flex-col items-center">
+          {employee.schedule != null && (
             <h3 className="font-bold">14:30 - 15:30</h3>
-            <span className="text-xs">Meal break</span>
+          )}
+          <span className="text-xs">Meal break</span>
         </div>
       </Card>
 

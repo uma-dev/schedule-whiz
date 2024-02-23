762b985abcf03851adf39e0f71380249701f91f3
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-02-22 23:55:26 -0600

    New axios config expired tokens proof

--- schedulewhiz-webapp/src/api/axiosConfig.tsx
+++ schedulewhiz-webapp/src/api/axiosConfig.tsx
@@ -1,7 +1,15 @@
 import axios from "axios";
+const BASE_URL = "http://192.168.3.110:8090";
 
 const api = axios.create({
-  baseURL: "http://192.168.3.110:8090",
+  baseURL: BASE_URL,
+});
+
+// Attach interceptor to this api
+export const apiPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 
 export default api;

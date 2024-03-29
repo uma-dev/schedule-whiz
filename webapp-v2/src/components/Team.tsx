122495b01a8724e728e73211a9d56657c126d8c7
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-03-29 13:54:35 -0600

    feat(my-team): added my team page

--- webapp-v2/src/components/Team.tsx
+++ webapp-v2/src/components/Team.tsx
@@ -1,7 +1,28 @@
-import { Heading } from "@chakra-ui/react";
+import { Flex, Heading, Select } from "@chakra-ui/react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import RecordsCalendar from "./calendar/RecordsCalendar";
 
 function Team() {
-  return <Heading>The team</Heading>;
+  return (
+    <Flex
+      direction={"column"}
+      height="100%"
+      width="100%"
+      maxWidth="900px"
+      p={{ base: 3, md: 8 }}
+      gap={6}
+      mx="auto"
+    >
+      <Heading variant="section-title">Team</Heading>
+      <Select placeholder="Select an member" variant="Flushed" bg="#f0eee1">
+        <option value="option1">Member 1</option>
+        <option value="option1">Member 2</option>
+        <option value="option1">Member 3</option>
+      </Select>
+
+      <RecordsCalendar />
+    </Flex>
+  );
 }
 
 export default Team;

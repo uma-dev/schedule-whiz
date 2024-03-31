aa358591da8cdf8192a0e642cd1874dd1707c698
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-03-30 21:37:42 -0600

    feat(login): added pwd and email validation

--- webapp-v2/src/components/Login.tsx
+++ webapp-v2/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import GradientDiv from "./GradientDiv";
 import {
   Flex,
@@ -16,13 +16,73 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import useAuth from "../hooks/useAuth";
+import { useEffect, useRef, useState } from "react";
+import { IoCheckmark, IoInformationCircleOutline } from "react-icons/io5";
+
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 function Login() {
-  const navigate = useNavigate();
   const { colorMode } = useColorMode();
+  // const { login } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleLogin = () => {
-    navigate("/dashboard");
+  const from = location.state?.from?.pathname || "/dashboard";
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+
+  const userRef = useRef<HTMLInputElement | null>(null);
+  const errRef = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    userRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(email));
+  }, [email]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+  }, [pwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    // if button enabled with JS hack
+    e.preventDefault();
+    const emailValidation = EMAIL_REGEX.test(email);
+    const pwdValidation = PWD_REGEX.test(pwd);
+
+    if (!emailValidation || !pwdValidation) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+
+    try {
+      // Get access token
+      const accessToken = "accessToken";
+      const refreshToken = "accessToken";
+      // Set user email and access token to authorize
+      // login(accessToken, refreshToken, userEmail);
+      // Post a record every login, backend will validate hour and only one record each day
+      // Go to the path which user came from
+      navigate(from, { replace: true });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -34,7 +94,13 @@ function Login() {
         transition={{ duration: 1 }}
       >
         <Flex minH={"85vh"} align={"center"} justify={"center"}>
-          <Stack spacing={6} mx={"auto"} minW={"sm"} maxW={"lg"} p={6}>
+          <Stack
+            spacing={6}
+            mx={"auto"}
+            minW={{ base: "sm", md: "md" }}
+            maxW={"lg"}
+            p={6}
+          >
             <Stack align={"center"}>
               <Heading fontSize={"4xl"} textAlign="center">
                 Sign in
@@ -49,14 +115,100 @@ function Login() {
               boxShadow={"lg"}
               p={8}
             >
+              <p
+                ref={errRef}
+                className={errMsg ? "errmsg" : "offscreen"}
+                aria-live="assertive"
+              >
+                {errMsg}
+              </p>
               <Stack spacing={4}>
                 <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
-                  <Input type="email" />
+                  <FormLabel
+                    display="flex"
+                    flexDirection="row"
+                    htmlFor="floating_email"
+                    gap={2}
+                  >
+                    Email address
+                    {validEmail && <IoCheckmark />}
+                  </FormLabel>
+                  <Input
+                    type="email"
+                    name="floating_email"
+                    id="email"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    aria-invalid={validEmail ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
+                  />
+
+                  {emailFocus && email && !validEmail && (
+                    <FormLabel
+                      id="uidnote"
+                      display="flex"
+                      flexDirection="row"
+                      gap={2}
+                      justifyContent="start"
+                      alignItems="center"
+                      py={1}
+                      color="red.700"
+                      fontSize={14}
+                    >
+                      <IoInformationCircleOutline />
+                      Must be a valid email address
+                    </FormLabel>
+                  )}
                 </FormControl>
+
                 <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
-                  <Input type="password" />
+                  <FormLabel
+                    display="flex"
+                    flexDirection="row"
+                    htmlFor="floating_password"
+                    gap={2}
+                  >
+                    Password
+                    {validPwd && <IoCheckmark />}
+                  </FormLabel>
+                  <Input
+                    type="password"
+                    name="floating_password"
+                    id="password"
+                    placeholder=" "
+                    required
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    aria-invalid={validPwd ? "false" : "true"}
+                    aria-describedby="pwdnote"
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                  />
+                  {pwdFocus && !validPwd && (
+                    <FormLabel
+                      id="pwdnote"
+                      display="flex"
+                      flexDirection="row"
+                      gap={2}
+                      justifyContent="center"
+                      alignItems="start"
+                      py={1}
+                      color="red.700"
+                      fontSize={14}
+                      maxWidth="xs"
+                      pb={0}
+                    >
+                      <IoInformationCircleOutline size={28} />
+                      <Text>
+                        Must include upper and lower case letters, a number and
+                        a special character.
+                      </Text>
+                    </FormLabel>
+                  )}
                 </FormControl>
                 <Stack spacing={10}>
                   <Stack
